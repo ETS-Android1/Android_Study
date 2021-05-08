db3e71ea19d78c2b12822c1d449c052ef3774983
Enterprise09 <goldenstaytes@gmail.com>
2021-05-08 18:13:55 +0900

    RecyclerView Complete

--- RecyclerViewExample/app/src/main/java/com/example/recyclerviewexample/MainActivity.java
+++ RecyclerViewExample/app/src/main/java/com/example/recyclerviewexample/MainActivity.java
@@ -1,17 +1,44 @@
 package com.example.recyclerviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<MainData> arrayList;
+    private  MainAdapter mainAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        arrayList = new ArrayList<>();
+        mainAdapter = new MainAdapter(arrayList);
+        recyclerView.setAdapter(mainAdapter);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainData mainData = new MainData(R.mipmap.ic_launcher, "Enterprise09", "RecyclerView");
+                arrayList.add(mainData);
+                mainAdapter.notifyDataSetChanged();
+            }
+        });
         
     }
 }
