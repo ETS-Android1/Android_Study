228e4b24eb960f2d6e50a2860f188ee17e5c7789
Enterprise09 <goldenstaytes@gmail.com>
2021-05-11 23:25:05 +0900

    Complete DialogExample Project

--- DialogExample/app/src/main/java/com/example/dialogexample/MainActivity.java
+++ DialogExample/app/src/main/java/com/example/dialogexample/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.dialogexample;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,7 +26,31 @@ public class MainActivity extends AppCompatActivity {
         dialogBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setIcon(R.mipmap.ic_launcher);
+                ad.setTitle("TITLE");
+                ad.setMessage("Who is Enterprise?");
 
+                final EditText et = new EditText(MainActivity.this);
+                ad.setView(et);
+
+                ad.setPositiveButton("Enter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String result = et.getText().toString();
+                        tvResult.setText(result);
+                        dialog.dismiss();
+                    }
+                });
+
+                ad.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                ad.show();
             }
         });
 
