5151bbc8b2664bbe77aec0c8be09502a6bba2066
Enterprise09 <goldenstaytes@gmail.com>
2021-05-11 23:15:00 +0900

    XML ID Update & MainActivity Instance Define

--- DialogExample/app/src/main/java/com/example/dialogexample/MainActivity.java
+++ DialogExample/app/src/main/java/com/example/dialogexample/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.dialogexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button dialogBtn;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dialogBtn = (Button) findViewById(R.id.dialogBtn);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        dialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
