d9e7534a8a947dbdf8d9d8ba88d384f3647026b9
Enterprise09 <goldenstaytes@gmail.com>
2021-05-12 23:20:01 +0900

    Create raw Dir & Import Music

--- ServiceExample/app/src/main/java/com/example/serviceexample/MusicService.java
+++ ServiceExample/app/src/main/java/com/example/serviceexample/MusicService.java
@@ -2,16 +2,36 @@ package com.example.serviceexample;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
 public class MusicService extends Service {
+
+    MediaPlayer mediaPlayer;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-
-
         return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.healer);
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
