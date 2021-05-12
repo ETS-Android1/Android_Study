e2f6d8090a1a9e7865ab9c113e7c11d0204be0cf
Enterprise09 <goldenstaytes@gmail.com>
2021-05-12 23:15:04 +0900

    Create MainActivity & MusicService

--- ServiceExample/app/src/main/java/com/example/serviceexample/MainActivity.java
+++ ServiceExample/app/src/main/java/com/example/serviceexample/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.serviceexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startBtn, endBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = (Button) findViewById(R.id.startBtn);
+        endBtn = (Button) findViewById(R.id.endBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        endBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
