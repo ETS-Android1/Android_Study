264794d010197d2569dccbad9f386d68ef231bec
Enterprise09 <goldenstaytes@gmail.com>
2021-05-13 13:17:23 +0900

    Get XML Instance At MainActivity

--- VideoRecordExample/app/src/main/java/com/example/videorecordexample/MainActivity.java
+++ VideoRecordExample/app/src/main/java/com/example/videorecordexample/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.videorecordexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Camera;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private Camera camera;
+    private MediaPlayer mediaPlayer;
+    private Button recordBtn;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private boolean recording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
     }
 }
