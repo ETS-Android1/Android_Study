d0fc22469ad104b69df2a345e2cf6af62a1507cc
Enterprise09 <goldenstaytes@gmail.com>
2021-05-13 14:06:58 +0900

    Contain Camera1 Class Error

--- VideoRecordExample/app/src/main/java/com/example/videorecordexample/MainActivity.java
+++ VideoRecordExample/app/src/main/java/com/example/videorecordexample/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.hardware.Camera;
-import android.media.MediaPlayer;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -20,7 +22,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     private Camera camera;
-    private MediaPlayer mediaPlayer;
+    private MediaRecorder mediaRecorder;
     private Button recordBtn;
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
@@ -40,12 +42,60 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
                         Manifest.permission.WRITE_EXTERNAL_STORAGE, 
                         Manifest.permission.RECORD_AUDIO)
                 .check();
+
+        recordBtn = (Button) findViewById(R.id.recordBtn);
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(recording){
+                    mediaRecorder.stop();
+                    mediaRecorder.release();
+                    camera.lock();
+                    recording = false;
+                    Toast.makeText(MainActivity.this, "녹화를 종료했습니다.", Toast.LENGTH_SHORT).show();
+                }else{
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "녹화가 시작되었습니다.", Toast.LENGTH_SHORT).show();
+                            try{
+                                mediaRecorder = new MediaRecorder();
+                                camera.unlock();
+                                mediaRecorder.setCamera(camera);
+                                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+                                mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                                mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_720P));
+                                mediaRecorder.setOrientationHint(90);
+                                mediaRecorder.setOutputFile("/test.mp4");
+                                mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+                                mediaRecorder.prepare();
+                                mediaRecorder.start();
+                                recording = true;
+                            }catch (Exception e){
+                                e.printStackTrace();
+                                mediaRecorder.release();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
+
     }
     
     PermissionListener permission = new PermissionListener() {
         @Override
         public void onPermissionGranted() {
             Toast.makeText(MainActivity.this, "권한 허가", Toast.LENGTH_SHORT).show();
+            camera = Camera.open();
+            camera.setDisplayOrientation(90);
+            surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+            surfaceHolder = surfaceView.getHolder();
+            surfaceHolder.addCallback(MainActivity.this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+            recording = false;
         }
 
         @Override
@@ -59,9 +109,27 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
     }
 
+    private void refreshCamera(Camera camera) {
+        if(surfaceHolder.getSurface() == null){
+            return;
+        }
+
+        try{
+            camera.stopPreview();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        setCamera(camera);
+    }
+
+    private void setCamera(Camera cam) {
+        camera = cam;
+    }
+
     @Override
     public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-
+        refreshCamera(camera);
     }
 
     @Override
