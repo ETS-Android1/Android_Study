b894c5c6c69a6e8429aef1cff3bd61fa66e48d29
Enterprise09 <goldenstaytes@gmail.com>
2021-05-13 13:23:06 +0900

    Create Permission Linstener

--- VideoRecordExample/app/src/main/java/com/example/videorecordexample/MainActivity.java
+++ VideoRecordExample/app/src/main/java/com/example/videorecordexample/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.videorecordexample;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.hardware.Camera;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
@@ -24,9 +31,28 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        TedPermission.with(this)
+                .setPermissionListener(permission)
+                .setRationaleMessage("녹화를 위하여 권한을 허용해주세요.")
+                .setDeniedMessage("권한이 거부되었습니다. 설정 > 권한에서 허용해주세요.")
+                .setPermissions(
+                        Manifest.permission.CAMERA, 
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE, 
+                        Manifest.permission.RECORD_AUDIO)
+                .check();
     }
+    
+    PermissionListener permission = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            Toast.makeText(MainActivity.this, "권한 허가", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+            Toast.makeText(MainActivity.this, "권한 거부", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
