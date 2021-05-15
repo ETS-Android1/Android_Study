793a640d21f6aab0af1bcf81d84339a5df0a0baa
Enterprise09 <goldenstaytes@gmail.com>
2021-05-15 14:17:55 +0900

    Get Instance at MainActivity.java

--- LayoutCustomExample/app/src/main/java/com/example/layoutcustomexample/MainActivity.java
+++ LayoutCustomExample/app/src/main/java/com/example/layoutcustomexample/MainActivity.java
@@ -1,14 +1,97 @@
 package com.example.layoutcustomexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+/*
+    drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);     //Define DrawerLayout
+        drawerView = (View) findViewById(R.id.drawer);
 
+        Button openBtn = (Button) findViewById(R.id.openBtn);
+        openBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+
+                return true;
+            }
+        });
+
+        Button closeBtn = (Button) findViewById(R.id.closeBtn);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+    }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
+*/
 public class MainActivity extends AppCompatActivity {
+
+    Button openBtn, closeBtn;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openBtn = (Button) findViewById(R.id.openBtn);
+        closeBtn = (Button) findViewById(R.id.closeBtn);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerContent);
+
+        openBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!drawerLayout.isDrawerOpen(Gravity.LEFT)){
+                   drawerLayout.openDrawer(Gravity.LEFT);
+                }
+            }
+        });
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(drawerLayout.isDrawerOpen(Gravity.LEFT)){
+                    drawerLayout.closeDrawer(Gravity.LEFT);
+                }
+            }
+        });
+
     }
 }
