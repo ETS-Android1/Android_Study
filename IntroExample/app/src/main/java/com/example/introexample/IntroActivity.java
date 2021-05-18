7baaf20e74296b5a2f32e5702d7720e995c39c38
Enterprise09 <goldenstaytes@gmail.com>
2021-05-18 22:19:03 +0900

    Complete IntroActivity.java

--- IntroExample/app/src/main/java/com/example/introexample/IntroActivity.java
+++ IntroExample/app/src/main/java/com/example/introexample/IntroActivity.java
@@ -2,13 +2,29 @@ package com.example.introexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class IntroActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        handler = new Handler();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        handler.postDelayed(r, 2000);
+
     }
 }
