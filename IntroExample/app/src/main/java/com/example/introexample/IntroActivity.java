5e4a2ed1bac7cd5226197fe0970de3c8eab6fe84
Enterprise09 <goldenstaytes@gmail.com>
2021-05-19 12:03:08 +0900

    Complete Project _ Running

--- IntroExample/app/src/main/java/com/example/introexample/IntroActivity.java
+++ IntroExample/app/src/main/java/com/example/introexample/IntroActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
 
         handler = new Handler();
         Runnable r = new Runnable() {
@@ -26,6 +29,6 @@ public class IntroActivity extends AppCompatActivity {
             }
         };
         handler.postDelayed(r, 2000);
-
+        
     }
 }
