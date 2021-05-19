b5c22fa30c355894fe50cb96dbcbee693f22ae8b
Enterprise09 <goldenstaytes@gmail.com>
2021-05-19 14:47:01 +0900

    Set IntroActivity Time

--- LayoutCustomExample/app/src/main/java/com/example/layoutcustomexample/IntroActivity.java
+++ LayoutCustomExample/app/src/main/java/com/example/layoutcustomexample/IntroActivity.java
@@ -2,13 +2,29 @@ package com.example.layoutcustomexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class IntroActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, 3000);
+
     }
 }
