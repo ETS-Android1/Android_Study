20d728faae33b567fc30ec88b7fe2261a4ad41d0
Enterprise09 <goldenstaytes@gmail.com>
2021-05-19 21:37:54 +0900

    Construct MyFirebaseMessagingService.java

--- FCMTest/app/src/main/java/com/example/fcmtest/MyFirebaseMessagingService.java
+++ FCMTest/app/src/main/java/com/example/fcmtest/MyFirebaseMessagingService.java
@@ -1,4 +1,59 @@
 package com.example.fcmtest;
 
-public class MyFirebaseMessagingService {
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        //super.onNewToken(s);
+        Log.d("FCM Log", "Refreshed token : " + token);
+
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        //super.onMessageReceived(remoteMessage);
+        if(remoteMessage.getNotification() != null){
+            Log.d("FCM Log", "Message : " + remoteMessage.getNotification().getBody());
+            String msgBody = remoteMessage.getNotification().getBody();
+            String msgTitle = remoteMessage.getNotification().getTitle();
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+            String channelId = "Channel ID";
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder =
+                    new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(msgTitle)
+                        .setContentText(msgBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                String channelName = "Channel Name";
+                NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+                notificationManager.createNotificationChannel(channel);
+            }
+            notificationManager.notify(0, notificationBuilder.build());
+        }
+
+
+    }
 }
