e4bf4bcb1eecf347c5e7041f5e7db98edf287ce7
Enterprise09 <goldenstaytes@gmail.com>
2021-05-20 15:19:11 +0900

    Update Messaging Service Class

--- FCMTest/app/src/main/java/com/example/fcmtest/MyFirebaseMessagingService.java
+++ FCMTest/app/src/main/java/com/example/fcmtest/MyFirebaseMessagingService.java
@@ -1,16 +1,12 @@
 package com.example.fcmtest;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -18,42 +14,35 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
+    private static final String TAG = "FirebaseMsgService";
+
+    private String msg, title;
+
     @Override
-    public void onNewToken(@NonNull String token) {
-        //super.onNewToken(s);
-        Log.d("FCM Log", "Refreshed token : " + token);
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.e(TAG,"onMessageReceived");
+
+        title = remoteMessage.getNotification().getTitle();
+        msg = remoteMessage.getNotification().getBody();
+
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this,0,new Intent(this,MainActivity.class),0);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setVibrate(new long[]{1,1000});
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0,mBuilder.build());
+
+        mBuilder.setContentIntent(contentIntent);
 
     }
 
-    @Override
-    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        //super.onMessageReceived(remoteMessage);
-        if(remoteMessage.getNotification() != null){
-            Log.d("FCM Log", "Message : " + remoteMessage.getNotification().getBody());
-            String msgBody = remoteMessage.getNotification().getBody();
-            String msgTitle = remoteMessage.getNotification().getTitle();
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-            String channelId = "Channel ID";
-            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            NotificationCompat.Builder notificationBuilder =
-                    new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle(msgTitle)
-                        .setContentText(msgBody)
-                        .setAutoCancel(true)
-                        .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent);
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                String channelName = "Channel Name";
-                NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-                notificationManager.createNotificationChannel(channel);
-            }
-            notificationManager.notify(0, notificationBuilder.build());
-        }
-
-
-    }
 }
