c3342dd206135042f250f64bc8cbd4ad3fc6079d
Enterprise09 <goldenstaytes@gmail.com>
2021-05-20 15:26:44 +0900

    Delete All

--- FCMTest/app/src/main/java/com/example/fcmtest/FirebaseinstanceIDServices.java
+++ FCMTest/app/src/main/java/com/example/fcmtest/FirebaseinstanceIDServices.java
@@ -1,26 +1,7 @@
 package com.example.fcmtest;
 
-import android.util.Log;
+public class FirebaseinstanceIDServices {
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
 
-public class FirebaseinstanceIDServices extends FirebaseInstanceIdService {
-
-    private static final String TAG = "MyFirebaseIIDService";
-
-    @Override
-    public void onTokenRefresh() {
-
-        String token = FirebaseInstanceId.getInstance().getToken();
-        Log.e(TAG,token);
-
-        sendRegistrationToServer(token);
-
-    }
-
-    private void sendRegistrationToServer (String token){
-
-    }
 
 }
