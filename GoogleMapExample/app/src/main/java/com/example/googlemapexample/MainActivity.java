8778183b7d8146c2eba3bacadabd4b455c0ad471
Enterprise09 <goldenstaytes@gmail.com>
2021-05-21 20:17:51 +0900

    Update Fragment ID

--- GoogleMapExample/app/src/main/java/com/example/googlemapexample/MainActivity.java
+++ GoogleMapExample/app/src/main/java/com/example/googlemapexample/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.googlemapexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.googleMap);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
