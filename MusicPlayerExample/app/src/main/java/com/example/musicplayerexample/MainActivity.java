be9dfaeb494f19a3cd4f664595519007402642ba
Enterprise09 <goldenstaytes@gmail.com>
2021-05-21 19:52:52 +0900

    Complete Project

--- MusicPlayerExample/app/src/main/java/com/example/musicplayerexample/MainActivity.java
+++ MusicPlayerExample/app/src/main/java/com/example/musicplayerexample/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.musicplayerexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startBtn, stopBtn;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = (Button) findViewById(R.id.startBtn);
+        stopBtn = (Button) findViewById(R.id.stopBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.healer);
+                mediaPlayer.start();
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                }
+
+            }
+        });
+
+
+
     }
 }
