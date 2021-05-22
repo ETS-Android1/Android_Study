e80f815b05cd6d160362b4ef624084043bfbc127
Enterprise09 <goldenstaytes@gmail.com>
2021-05-22 17:13:33 +0900

    Design SubActivity

--- ComeBackExample/app/src/main/java/com/example/comebackexample/SubActivity.java
+++ ComeBackExample/app/src/main/java/com/example/comebackexample/SubActivity.java
@@ -2,13 +2,33 @@ package com.example.comebackexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SubActivity extends AppCompatActivity {
+
+    private EditText et_comeback;
+    private Button closeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+        
+        et_comeback = (EditText) findViewById(R.id.et_comeback);
+        closeBtn = (Button) findViewById(R.id.closeBtn);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("comeback", et_comeback.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();       //Close SubActivity
+            }
+        });//end of OnClickListener
+        
     }
 }
