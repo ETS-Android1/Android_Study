4d1330d2609aa20ababd4365cb851e72c6758fe7
Enterprise09 <goldenstaytes@gmail.com>
2021-05-22 17:17:51 +0900

    End Project _ Comeback

--- ComeBackExample/app/src/main/java/com/example/comebackexample/MainActivity.java
+++ ComeBackExample/app/src/main/java/com/example/comebackexample/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.comebackexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_comeback;
+    private Button goBtn;
+
+    private static final int REQUEST_CODE = 17;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_comeback = (TextView) findViewById(R.id.tv_comeback);
+        goBtn = (Button) findViewById(R.id.goBtn);
+        goBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });//end of OnClickListener
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "FALSE", Toast.LENGTH_SHORT).show();
+        }
+
+        if(requestCode == REQUEST_CODE){
+            String resultText = data.getStringExtra("comeback");
+            tv_comeback.setText(resultText);
+        }
+
     }
 }
