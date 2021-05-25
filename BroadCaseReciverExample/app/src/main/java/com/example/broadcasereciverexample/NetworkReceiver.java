9cdc4e7de50423c20766de705f4c335ba06030a0
Enterprise09 <goldenstaytes@gmail.com>
2021-05-25 22:57:25 +0900

    Override onReceive

--- BroadCaseReciverExample/app/src/main/java/com/example/broadcasereciverexample/NetworkReceiver.java
+++ BroadCaseReciverExample/app/src/main/java/com/example/broadcasereciverexample/NetworkReceiver.java
@@ -1,4 +1,24 @@
 package com.example.broadcasereciverexample;
 
-public class NetworkReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+
+public class NetworkReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Get Network State
+        if(WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.getAction())){
+            NetworkInfo info = (NetworkInfo) intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+            NetworkInfo.DetailedState state = info.getDetailedState();
+            if(state == NetworkInfo.DetailedState.CONNECTED){   //Connect State
+
+            }
+            else if(state == NetworkInfo.DetailedState.DISCONNECTED){   //DisConnect State
+
+            }
+        }
+    }
 }
