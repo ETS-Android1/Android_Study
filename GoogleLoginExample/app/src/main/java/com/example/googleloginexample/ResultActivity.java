f796d3c72db944df629259303674e58cbe383c49
Enterprise09 <goldenstaytes@gmail.com>
2021-05-28 16:51:19 +0900

    Implement ResultActivity

--- GoogleLoginExample/app/src/main/java/com/example/googleloginexample/ResultActivity.java
+++ GoogleLoginExample/app/src/main/java/com/example/googleloginexample/ResultActivity.java
@@ -2,13 +2,32 @@ package com.example.googleloginexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView tv_result;
+    private ImageView iv_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String nickName = intent.getStringExtra("nickName");
+        String photoUrl = intent.getStringExtra("photoUrl");
+
+        tv_result = (TextView) findViewById(R.id.tv_result);
+        iv_profile = (ImageView) findViewById(R.id.iv_pofile);
+
+        tv_result.setText(nickName);
+        Glide.with(this).load(photoUrl).into(iv_profile);   //Display Profile Photo
+
     }
 }
