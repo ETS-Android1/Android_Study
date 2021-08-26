4bc91082f086f8159237207bb9f898258add2cbb
Enterprise09 <goldenstaytes@gmail.com>
2021-08-26 23:06:16 +0900

    upload file to FireStorage

--- FirestoreTest/app/src/main/java/com/example/firestoretest/MainActivity.java
+++ FirestoreTest/app/src/main/java/com/example/firestoretest/MainActivity.java
@@ -1,32 +1,89 @@
 package com.example.firestoretest;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore fstore = FirebaseFirestore.getInstance();
+    FirebaseStorage fstorage = FirebaseStorage.getInstance();
+    StorageReference storageRef = fstorage.getReference();
     Map<String, Object> user = new HashMap<>();
-    Button addBtn, readBtn;
+    Uri filePath;
+    Button addBtn, readBtn, selectBtn;
     TextView tv1, tv2, tv3;
+    ImageView iv;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0 && resultCode == RESULT_OK && data != null &&data.getData() != null){
+            filePath = data.getData();
+            iv.setImageURI(filePath);
+            uploadFile();
+        }
+    }
+
+    private void uploadFile(){
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Uploading Image...");
+        pd.show();
+
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference riversRef = storageRef.child("images/" + randomKey);
+
+        riversRef.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                pd.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "failed uploading", Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double progressPercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                pd.setMessage("Percentage: " + (int)progressPercent + "%");
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +93,9 @@ public class MainActivity extends AppCompatActivity {
         tv1 = (TextView) findViewById(R.id.textView1);
         tv2 = (TextView) findViewById(R.id.textView2);
         tv3 = (TextView) findViewById(R.id.textView3);
+        iv = (ImageView) findViewById(R.id.iv);
 
+        //upload data
         addBtn = (Button) findViewById(R.id.addBtn);
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,9 +115,27 @@ public class MainActivity extends AppCompatActivity {
                         Log.w("Firestore", "Error adding document", e);
                     }
                 });
+
+                uploadFile();
+
             }
         });
 
+        //select Photo
+        selectBtn = (Button)findViewById(R.id.selectBtn);
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+
+        //read data
         readBtn = (Button) findViewById(R.id.readBtn);
         readBtn.setOnClickListener(new View.OnClickListener() {
             @Override
