9ec9575f364fcf80b939d8b520bcf324952cd44c
Enterprise09 <goldenstaytes@gmail.com>
2021-09-07 19:12:00 +0900

    Display img from firestorage

--- FirestoreTest/app/src/main/java/com/example/firestoretest/MainActivity.java
+++ FirestoreTest/app/src/main/java/com/example/firestoretest/MainActivity.java
@@ -6,23 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -31,9 +28,6 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-
-import java.io.IOException;
-import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -106,7 +100,6 @@ public class MainActivity extends AppCompatActivity {
                 product.put("요약정보", et3.getText().toString());
                 product.put("이미지 링크", filePath.toString());
 
-
                 fstore.collection("product").add(product).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
@@ -136,8 +129,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
         //read data
         readBtn = (Button) findViewById(R.id.readBtn);
         readBtn.setOnClickListener(new View.OnClickListener() {
@@ -152,11 +143,22 @@ public class MainActivity extends AppCompatActivity {
                                 et1.setText(document.get("제품명").toString());
                                 et2.setText(document.get("가격").toString());
                                 et3.setText(document.get("요약정보").toString());
-                                iv.setImageURI(Uri.parse(document.get("이미지 링크").toString()));
                             }
                         }else{
                             Log.w("Firestore", "Error getting Document", task.getException());
                         }
+                    }
+                });
+                storageRef.child("images/253d6a07-5501-4c48-967d-e259eb58fcfc").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Log.d("getURL", "success");
+                        Glide.with(getApplicationContext()).load(uri).into(iv);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("getURL", "Fail");
                     }
                 });
             }
