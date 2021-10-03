bc3e50f6ef2087b305fece37fc2bda0941e68124
Enterprise09 <goldenstaytes@gmail.com>
2021-10-03 21:11:40 +0900

    clear code for login activity

--- GoogleLoginExample/app/src/main/java/com/example/googleloginexample/LoginActivity.java
+++ GoogleLoginExample/app/src/main/java/com/example/googleloginexample/LoginActivity.java
@@ -1,14 +1,114 @@
 package com.example.googleloginexample;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener, GoogleApiClient.OnConnectionFailedListener {
+    private SignInButton btn_google;                    //Login Button
+    private FirebaseAuth auth;                          //Auth
+    private GoogleApiClient googleApiClient;            //Google API Client
+    private static final int REQ_SIGN_GOOGLE = 100;     //Result Code of Google Login
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+        auth = FirebaseAuth.getInstance();      //Get Auth Instance
+
+        btn_google = findViewById(R.id.btn_google);
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);      //Go to Google Login Interface
+                startActivityForResult(intent, REQ_SIGN_GOOGLE);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQ_SIGN_GOOGLE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){     //Check Auth State
+                GoogleSignInAccount account = result.getSignInAccount();    //Get Every Login Information(Photo, ID, Email, PW...)
+                resultLogin(account);   //Print Login Information
+            }else{
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void resultLogin(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {    //Check Login State
+                        if(task.isSuccessful()){    //Login Success
+                            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                            intent.putExtra("nickName", account.getDisplayName().toString());
+                            intent.putExtra("photoUrl", String.valueOf(account.getPhotoUrl()));     //Change to String
+                            startActivity(intent);      //Go to ResultActivity
+                            finish();
+                        }else{  //Get data fail
+                            Toast.makeText(getApplicationContext(), "Get data fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
